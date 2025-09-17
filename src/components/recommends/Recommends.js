36f8df2ef4bb13b5e14b9b19c42d9a2dd9ac86da
daniel rozenblat <danielroz12345@gmail.com>
2025-09-17 17:02:13 +0300

    newChnage

--- src/components/recommends/Recommends.js
+++ src/components/recommends/Recommends.js
@@ -1,14 +1,11 @@
 import styles from "./Recommends.module.css"
 import result1 from "../../images/ענבל רוזנפלד תלמידות 1.png"
 import result2 from "../../images/ענבל רוזנפלד תלמידות 2.png"
-import result3 from "../../images/ענבל רוזנפלד תלמידות 3.png"
-import result4 from "../../images/ענבל רוזנפלד תלמידות 4.png"
-import result5 from "../../images/ענבל רוזנפלד תלמידות 5.png"
-import result6 from "../../images/ענבל רוזנפלד תלמידות 6.png"
+
 import result7 from "../../images/ענבל רוזנפלד תלמידות 7.png"
 import result8 from "../../images/ענבל רוזנפלד תלמידות 8.png"
 import result9 from "../../images/ענבל רוזנפלד תלמידות 9.png"
-import result10 from "../../images/ענבל רוזנפלד תלמידות 10.png"
+
 import result11 from "../../images/ענבל רוזנפלד המלצה 1.png"
 import result12 from "../../images/ענבל רוזנפלד המלצה 2.png"
 import result13 from "../../images/ענבל רוזנפלד המלצה 3.png"
@@ -53,10 +50,7 @@ const Recommends=()=>{
           type: 'image',
           src: result8,
         },
-        {
-          type: 'image',
-          src: result10,
-        },
+      
         {
           type: 'image',
           src: result9,
@@ -69,26 +63,13 @@ const Recommends=()=>{
           type: 'image',
           src: result2,
         },
-        {
-          type: 'image',
-          src: result3,
-        },
-        {
-          type: 'image',
-          src: result4,
-        }, 
+        
         {
           type: 'image',
           src: result12,
         },
-        {
-          type: 'image',
-          src: result5,
-        },
-          {
-            type: 'image',
-            src: result6,
-          },
+     
+        
           {
             type: 'image',
             src: result17,
--- src/components/recommends/Works.js
+++ src/components/recommends/Works.js
@@ -4,14 +4,11 @@ import styles from './Recommends.module.css';
 
 import result1 from "../../images/ענבל רוזנפלד תלמידות 1.png"
 import result2 from "../../images/ענבל רוזנפלד תלמידות 2.png"
-import result3 from "../../images/ענבל רוזנפלד תלמידות 3.png"
-import result4 from "../../images/ענבל רוזנפלד תלמידות 4.png"
-import result5 from "../../images/ענבל רוזנפלד תלמידות 5.png"
-import result6 from "../../images/ענבל רוזנפלד תלמידות 6.png"
+
 import result7 from "../../images/ענבל רוזנפלד תלמידות 7.png"
 import result8 from "../../images/ענבל רוזנפלד תלמידות 8.png"
 import result9 from "../../images/ענבל רוזנפלד תלמידות 9.png"
-import result10 from "../../images/ענבל רוזנפלד תלמידות 10.png"
+
 import result11 from "../../images/ענבל רוזנפלד המלצה 1.png"
 import result12 from "../../images/ענבל רוזנפלד המלצה 2.png"
 import result13 from "../../images/ענבל רוזנפלד המלצה 3.png"
@@ -25,8 +22,8 @@ import result19 from "../../images/ענבל רוזנפלד תלמידות 15.png
 
 const Works = () => {
   const images = [
-    result1, result2, result3, result4, 
-    result5, result6, result7, result8, result9, result10, result11 ,result12, result13, result14, result15, result16, result17, result19, result18
+    result1, result2, 
+    result7, result8, result9,  result11 ,result12, result13, result14, result15, result16, result17, result19, result18
   ];
 
   return (
